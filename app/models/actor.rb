bf5d6ba808f6ab62ef068b9e3404fb7021ebfb62
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-25 11:38:50 -0700

    commit tree cf1649fbdae1a955b834df25a85c77e85323fa46

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    new_array = []
+    self.characters.map do |character|
+        new_array << "#{character.name} - #{character.show.name}"
+    end
+    new_array
+  end
 end
